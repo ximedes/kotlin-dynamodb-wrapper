d4826d27fe81a34ea06aca5d3e96eb36fc1daef4
Mathijs den Burger <mathijs.den.burger@ximedes.com>
2020-09-18 16:51:09 +0200

    Support reading attributes of type Double

--- src/main/kotlin/com/ximedes/dynamodb/dsl/DynamoDbDSL.kt
+++ src/main/kotlin/com/ximedes/dynamodb/dsl/DynamoDbDSL.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2019-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ inline fun <reified T : Any?> Item.takeOrNull(key: String): T = when (T::class) 
     String::class -> get(key)?.s()
     Int::class -> get(key)?.n()?.toInt()
     Long::class -> get(key)?.n()?.toLong()
+    Double::class -> get(key)?.n()?.toDouble()
     Boolean::class -> get(key)?.bool()
     ByteArray::class -> get(key)?.b()?.asByteArray()
     else -> throw IllegalArgumentException("Unsupported type ${T::class}")
